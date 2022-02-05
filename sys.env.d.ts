726539df8b0befc07c1781ce8a8f9f7998edab7a
angeloreborn <75548885+angeloreborn@users.noreply.github.com>
2022-02-06 00:23:21 +0200

    Create Interfaces

--- sys.env.d.ts
+++ sys.env.d.ts
@@ -1,3 +1,48 @@
-interface Session{
-  
+interface Session extends Hash, AuthorizationClaims, User, Activity{ 
+  user: User
+  activity: Activity
+}
+
+interface User extends AuthorizationClaims, PermissionClaims{
+    username?: string, 
+    email?: string,
+    addresses?: Address[],
+}
+
+interface AuthorizationClaims{
+    authorizations: Authorization[]
+}
+
+interface PermissionClaims{
+    permissions: Permission[]
+}
+interface Address{
+
+}
+
+interface Authorization extends Hash{
+    token: string 
+}
+
+interface BrowserActivity extends Hash{
+    event: Event[]
+    collect_threshold: number 
+    user_agent: string
+}
+
+interface Event {
+    date: Date
+    action: Action<T>
+}
+
+interface Action<T>{
+
+}
+
+interface Hash{
+    hash: string
+}
+
+interface Permission{
+    hash:string
 }
