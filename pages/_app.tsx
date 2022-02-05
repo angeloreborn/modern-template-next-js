caf89e677ceaf00baeac8edb963e230d7ae08a71
angeloreborn <75548885+angeloreborn@users.noreply.github.com>
2022-02-05 23:53:26 +0200

    Create Interface - Session

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,10 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 
+interface Session{
+
+}
+
 function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
 }
