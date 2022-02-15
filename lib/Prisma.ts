fd970e956ca82a43dde1520ec845519e182d6e6c
angeloreborn <75548885+angeloreborn@users.noreply.github.com>
2022-02-15 22:29:53 +0200

    Setup Prisma Lib

---
+++ lib/Prisma.ts
@@ -0,0 +1,3 @@
+import { PrismaClient } from "../../prisma/modern-template-next-js/generated/client";
+
+export default new PrismaClient();
--- pages/api/user/authorize.ts
+++ pages/api/user/authorize.ts
@@ -1,7 +1,7 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next'
-import { PrismaClient } from '../../../../prisma/modern-template-next-js/generated/client'
-const prisma = new PrismaClient();
+import prisma from '../../../lib/Prisma'
+
 type Data = {
   name: string
 }
